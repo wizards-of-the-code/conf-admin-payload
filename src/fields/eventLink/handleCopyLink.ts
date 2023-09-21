89ccc2ab30caff67cd64da556f5b6a4c62551705
Lexeor <lexeor.ross@gmail.com>
2023-09-21 16:27:55 +0400

    add unsecuredCopyToClipboard function
    to allow copy to clipboard over HTTP instead of HTTPS

--- src/fields/eventLink/handleCopyLink.ts
+++ src/fields/eventLink/handleCopyLink.ts
@@ -1,8 +1,9 @@
 import { toast } from "react-toastify";
+import unsecuredCopyToClipboard from "../../utils/unsecuredCopyToClipboard";
 
 const handleCopyLink = (e: React.SyntheticEvent, param: string | number) => {
-  console.log('param', param);
-  navigator.clipboard.writeText(`https://t.me/ConfMerchantBot?start=${param}`);
+  // navigator.clipboard.writeText(`https://t.me/ConfMerchantBot?start=${param}`);
+  unsecuredCopyToClipboard(`https://t.me/ConfMerchantBot?start=${param}`);
   toast.success("Ссылка на бота с параметром конференции скопирована в буфер обмена!");
 }
 
