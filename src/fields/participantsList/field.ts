0b621e1b260a0b0c5e0310300c90245ab5bf1a53
Lexeor <lexeor.ross@gmail.com>
2023-09-28 14:17:15 +0400

    add hook to not to save changes to collection from ParticipantsList field

--- src/fields/participantsList/field.ts
+++ src/fields/participantsList/field.ts
@@ -8,8 +8,16 @@ const ParticipantsListField: Field = {
   admin: {
     components: {
       Field: ParticipantsListComponent,
-    }
-  }
+    },
+  },
+  hooks: {
+    beforeChange: [
+      ({ siblingData }) => {
+        // Ensures data is not stored in DB
+        delete siblingData['participants_list'];
+      }
+    ],
+  },
 };
 
 export default ParticipantsListField;
