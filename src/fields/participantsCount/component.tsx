bdd94d2838e9c6a101a5b840c91ba26d432133e0
Lexeor <lexeor.ross@gmail.com>
2023-10-05 11:23:13 +0400

    implement design for participants counter field

--- src/fields/participantsCount/component.tsx
+++ src/fields/participantsCount/component.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useDocumentInfo } from 'payload/components/utilities';
+import { Tooltip } from 'react-tooltip';
 
 const ParticipantsCountComponent: FC<{ path: string }> = ({ path }) => {
   const { id } = useDocumentInfo();
   const [count, setCount] = useState(0);
   const [paid, setPaid] = useState(0);
+  const [refund, setRefund] = useState(0);
 
   useEffect(() => {
     const fetchParticipants = async () => {
@@ -14,14 +16,17 @@ const ParticipantsCountComponent: FC<{ path: string }> = ({ path }) => {
         );
         const data = await response.json();
 
-        if(data.paid) {
+        if (data.paid) {
           setPaid(data.paid);
         }
-        
-        if(data.count) {
+
+        if (data.count) {
           setCount(data.count);
         }
 
+        if (data.refund) {
+          setRefund(data.refund);
+        }
       } catch (error) {
         console.log('error');
       }
@@ -33,7 +38,30 @@ const ParticipantsCountComponent: FC<{ path: string }> = ({ path }) => {
   return (
     <div>
       <h3>Участники</h3>
-      <div><b>{count}</b> / <b>{paid}</b></div>
+      <span className="participants-cell">
+        <Tooltip id="tooltip-participants" />
+        <span
+          className="registred-span"
+          data-tooltip-id="tooltip-participants"
+          data-tooltip-content="Регистрации"
+        >
+          {count}
+        </span>
+        <span
+          className="paid-span"
+          data-tooltip-id="tooltip-participants"
+          data-tooltip-content="Оплаты"
+        >
+          {paid}
+        </span>
+        <span
+          className="refund-span"
+          data-tooltip-id="tooltip-participants"
+          data-tooltip-content="Возвраты"
+        >
+          {refund}
+        </span>
+      </span>
     </div>
   );
 };
