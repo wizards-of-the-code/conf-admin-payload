a9af290ab25d2e8a8154ca95ef6f6bcec9506f9b
Lexeor <lexeor.ross@gmail.com>
2023-10-06 11:30:55 +0400

    add tooltip for event bot-link

--- src/fields/eventLink/cell.tsx
+++ src/fields/eventLink/cell.tsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React from 'react';
 import CopySvg from '../../assets/svgs/file-copy-line.svg';
-import "./index.scss";
-import handleCopyLink from "./handleCopyLink";
+import './index.scss';
+import handleCopyLink from './handleCopyLink';
+import { Tooltip } from 'react-tooltip';
 
-const baseClass = "copy-link-cell";
+const baseClass = 'copy-link-cell';
 
 type CustomCellProps = {
-  field: any, 
-  colIndex: any,
-  collection: any,
-  cellData: any,
-  rowData: any,
-}
+  field: any;
+  colIndex: any;
+  collection: any;
+  cellData: any;
+  rowData: any;
+};
 
 const EventLinkCell: React.FC<CustomCellProps> = (props) => {
   const { rowData } = props;
 
-  return <span className={baseClass}>
-    <img src={CopySvg} alt="Copy bot link" onClick={(e) => handleCopyLink(e, rowData.id)} /></span>;
+  return (
+    <span className={baseClass}>
+      <Tooltip id="tooltip-event-link" />
+      <img
+        src={CopySvg}
+        alt="Copy bot link"
+        data-tooltip-id="tooltip-event-link"
+        data-tooltip-content="Скопировать прямую ссылку на бота"
+        onClick={(e) => handleCopyLink(e, rowData.id)}
+      />
+    </span>
+  );
 };
 
 export default EventLinkCell;
