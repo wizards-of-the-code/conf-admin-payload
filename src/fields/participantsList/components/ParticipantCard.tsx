d27ea2e9f3229b75f273b72e466cbe48886a8f2b
Lexeor <lexeor.ross@gmail.com>
2023-10-09 12:06:05 +0400

    fix chevron button

--- src/fields/participantsList/components/ParticipantCard.tsx
+++ src/fields/participantsList/components/ParticipantCard.tsx
@@ -103,9 +103,21 @@ function ParticipantCard({ participant, eventData, paymentOptions }: Props) {
 
   // Renders
   const renderChevron = collapsed ? (
-    <img className="svg-icon-small svg-link" src={ChevronDown} />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path d="M11.9997 13.1714L16.9495 8.22168L18.3637 9.63589L11.9997 15.9999L5.63574 9.63589L7.04996 8.22168L11.9997 13.1714Z"></path>
+    </svg>
   ) : (
-    <img className="svg-icon-small svg-link" src={ChevronUp} />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path d="M11.9997 10.8284L7.04996 15.7782L5.63574 14.364L11.9997 8L18.3637 14.364L16.9495 15.7782L11.9997 10.8284Z"></path>
+    </svg>
   );
 
   const renderRole = () => {
